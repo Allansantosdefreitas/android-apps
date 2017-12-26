4d6b3feeee890a0c22e5cbc6d3dfe156f17cc8d5
Allan Santos <allan.allan.santos@gmail.com>
2017-12-26 11:56:35 -0300

    Pratica 05 (doing)

---
+++ práticas/prática 5 (doing)/Pratica05/app/src/main/java/br/edu/ifpe/tads/pdm/allansfreitas/pratica05/SignUpActivity.java
@@ -0,0 +1,45 @@
+package br.edu.ifpe.tads.pdm.allansfreitas.pratica05;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText edEmail;
+    EditText edPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edEmail = (EditText) findViewById(R.id.edit_email);
+        edPassword = (EditText) findViewById(R.id.edit_password);
+    }
+
+
+    public void buttonSignUpClick(View view) {
+        String email = edEmail.getText().toString();
+        String password = edPassword.getText().toString();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        String msg = task.isSuccessful() ? "SIGN UP OK!":
+                                "SIGN UP ERROR!";
+                        Toast.makeText(SignUpActivity.this, msg,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
