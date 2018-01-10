8a9ab97f5edbfe72e9ce811b86e4cbe9051d50e5
Allan Santos <allan.allan.santos@gmail.com>
2018-01-09 22:37:31 -0300

    AutenticarUsuario2(falta login auto)

--- apps-curso/AutenticacaoUsuario/app/src/main/java/br/edu/ifpe/tads/pdm/autenticacaousuario/MainActivity.java
+++ apps-curso/AutenticacaoUsuario/app/src/main/java/br/edu/ifpe/tads/pdm/autenticacaousuario/MainActivity.java
@@ -1,9 +1,13 @@
 package br.edu.ifpe.tads.pdm.autenticacaousuario;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,31 +18,88 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private EditText editEmail, editSenha;
+    private Button botaoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         firebaseAuth = FirebaseAuth.getInstance();
+        editEmail = (EditText) findViewById(R.id.emailId);
+        editSenha = (EditText) findViewById(R.id.senhaId);
+        botaoLogin = (Button) findViewById(R.id.botaoLoginId);
 
-//         firebaseAuth.signOut();
+
+        /* Já redireciona para a HomeActivity */
+        if ( estaLogado() == true ){
+
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            Toast.makeText(getApplicationContext(), "pegou :)", Toast.LENGTH_LONG).show();
+            //startActivity(intent);
+
+        }
+
+        botaoLogin.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                String emailDigitado, senhaDigitada;
+
+                emailDigitado = editEmail.getText().toString();
+                senhaDigitada = editSenha.getText().toString();
+
+                emailDigitado = "cassio@gmail.com";
+                senhaDigitada = "cassio123";
+
+               logarUsuario(emailDigitado, senhaDigitada);
+
+                if ( estaLogado() == true ){
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+
+
+                }
+            }
+        });
+
+        //estaLogado();
+
+        //cadastrarUsuario(emailDigitado, senhaDigitada);
+
+
+
+        //logoutUsuario();
+
+    }
+
+
+    private void logarUsuario(String email, String senha) {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+  //      final boolean loginOk;
 
         /* Login ----------------------------------------------- */
-        firebaseAuth.signInWithEmailAndPassword("sabrina@gmail.com", "sabrina123")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
 
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
                         if( task.isSuccessful() ){
 
-//                            Toast.makeText(getApplicationContext(), "sucesso login", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "sucesso login", Toast.LENGTH_SHORT).show();
                             Log.i("signInUser", "sucesso login usuário");
+        //                    loginOk = true;
 
                         }else{
-  //                          Toast.makeText(getApplicationContext(), "falha login", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "falha login " + task.getException(), Toast.LENGTH_SHORT).show();
                             Log.i("signInUser", "falha login usuário");
+//                            loginOk = false;
 
                         }
 
@@ -46,38 +107,138 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+    }
+
+
+    private void cadastrarUsuario(String email, String senha) {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+         /* Cadastro -------------------------------- */
+         firebaseAuth.createUserWithEmailAndPassword(email, senha)
+                 .addOnCompleteListener(MainActivity.this,
+                         new OnCompleteListener<AuthResult>() {
+
+                     @Override
+                     public void onComplete(@NonNull Task<AuthResult> task) {
+                         Toast.makeText(getApplicationContext(), "entrou", Toast.LENGTH_SHORT).show();
+
+                         if( task.isSuccessful() ){
+
+                             Toast.makeText(getApplicationContext(), "sucess", Toast.LENGTH_SHORT).show();
+                             Log.i("createUser", "Sucesso ao cadastrar usuário");
+                         }else{
+
+                            // getException é apenas para testes
+                             Toast.makeText(getApplicationContext(), "fail " + task.getException(), Toast.LENGTH_LONG).show();
+                             Log.i("createUser", "Falha ao cadastrar usuário");
+                         }
+                     }
+                 });
+
+    }
+
+    private Boolean estaLogado() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         if( firebaseAuth.getCurrentUser() != null ){
             Toast.makeText(getApplicationContext(), "Usuário logado", Toast.LENGTH_SHORT)
                     .show();
+            return true;
         }else{
             Toast.makeText(getApplicationContext(), "Usuário não logado", Toast.LENGTH_SHORT)
                     .show();
+            return false;
         }
-//         /* Cadastro -------------------------------- */
-//         firebaseAuth.createUserWithEmailAndPassword("betania@gmail.com", "betania123")
-//                 .addOnCompleteListener(MainActivity.this,
-//                         new OnCompleteListener<AuthResult>() {
-//
-//                     @Override
-//                     public void onComplete(@NonNull Task<AuthResult> task) {
-//                         Toast.makeText(getApplicationContext(), "entrou", Toast.LENGTH_SHORT).show();
-//
-//                         if( task.isSuccessful() ){
-//
-//                             Toast.makeText(getApplicationContext(), "sucess", Toast.LENGTH_SHORT).show();
-//                             Log.i("createUser", "Sucesso ao cadastrar usuário");
-//                         }else{
-//
-//                            // getException é apenas para testes
-//                             Toast.makeText(getApplicationContext(), "fail " + task.getException(), Toast.LENGTH_LONG).show();
-//                             Log.i("createUser", "Falha ao cadastrar usuário");
-//                         }
-//                     }
-//                 });
-
-
-
     }
 
 
 }
+
+
+/* #-------- OLD CODE  ------------------------OLD CODE--------------------------------OLD CODE-------- */
+//
+//package br.edu.ifpe.tads.pdm.autenticacaousuario;
+//
+//        import android.support.annotation.NonNull;
+//        import android.support.v7.app.AppCompatActivity;
+//        import android.os.Bundle;
+//        import android.util.Log;
+//        import android.widget.Toast;
+//
+//        import com.google.android.gms.tasks.OnCompleteListener;
+//        import com.google.android.gms.tasks.Task;
+//        import com.google.firebase.auth.AuthResult;
+//        import com.google.firebase.auth.FirebaseAuth;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    private FirebaseAuth firebaseAuth;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        firebaseAuth = FirebaseAuth.getInstance();
+//
+////         firebaseAuth.signOut();
+//
+//        /* Login ----------------------------------------------- */
+//        firebaseAuth.signInWithEmailAndPassword("sabrina@gmail.com", "sabrina123")
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//
+//                        if( task.isSuccessful() ){
+//
+////                            Toast.makeText(getApplicationContext(), "sucesso login", Toast.LENGTH_SHORT).show();
+//                            Log.i("signInUser", "sucesso login usuário");
+//
+//                        }else{
+//                            //                          Toast.makeText(getApplicationContext(), "falha login", Toast.LENGTH_SHORT).show();
+//                            Log.i("signInUser", "falha login usuário");
+//
+//                        }
+//
+//                    }
+//                });
+//
+//
+//        if( firebaseAuth.getCurrentUser() != null ){
+//            Toast.makeText(getApplicationContext(), "Usuário logado", Toast.LENGTH_SHORT)
+//                    .show();
+//        }else{
+//            Toast.makeText(getApplicationContext(), "Usuário não logado", Toast.LENGTH_SHORT)
+//                    .show();
+//        }
+////         /* Cadastro -------------------------------- */
+////         firebaseAuth.createUserWithEmailAndPassword("betania@gmail.com", "betania123")
+////                 .addOnCompleteListener(MainActivity.this,
+////                         new OnCompleteListener<AuthResult>() {
+////
+////                     @Override
+////                     public void onComplete(@NonNull Task<AuthResult> task) {
+////                         Toast.makeText(getApplicationContext(), "entrou", Toast.LENGTH_SHORT).show();
+////
+////                         if( task.isSuccessful() ){
+////
+////                             Toast.makeText(getApplicationContext(), "sucess", Toast.LENGTH_SHORT).show();
+////                             Log.i("createUser", "Sucesso ao cadastrar usuário");
+////                         }else{
+////
+////                            // getException é apenas para testes
+////                             Toast.makeText(getApplicationContext(), "fail " + task.getException(), Toast.LENGTH_LONG).show();
+////                             Log.i("createUser", "Falha ao cadastrar usuário");
+////                         }
+////                     }
+////                 });
+//
+//
+//
+//    }
+//
+//
+//}
