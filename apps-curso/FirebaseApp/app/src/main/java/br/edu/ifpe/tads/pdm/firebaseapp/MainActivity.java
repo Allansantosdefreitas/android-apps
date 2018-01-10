e19b295c558b9b661aef89c563ce9a72068c96dc
Allan Santos <allan.allan.santos@gmail.com>
2018-01-10 17:09:02 -0300

    FB: Salvando dados

--- apps-curso/FirebaseApp/app/src/main/java/br/edu/ifpe/tads/pdm/firebaseapp/MainActivity.java
+++ apps-curso/FirebaseApp/app/src/main/java/br/edu/ifpe/tads/pdm/firebaseapp/MainActivity.java
@@ -6,11 +6,19 @@ import android.os.Bundle;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import br.edu.ifpe.tads.pdm.firebaseapp.model.Produto;
+import br.edu.ifpe.tads.pdm.firebaseapp.model.Usuario;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance()
+    /* Referencia o nó Raiz (root) da estrutura de dados do Firebase ;) */
+    private DatabaseReference firebaseReferenciaRaiz = FirebaseDatabase.getInstance()
             .getReference();
+
+    /* Referencia, a partir do nó raíz, os sub nós */
+    private DatabaseReference usuariosReferenciaFB = firebaseReferenciaRaiz.child("usuarios");
+    private  DatabaseReference produtosReferenciaFB = firebaseReferenciaRaiz.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* Navega para o nó pontos (se não existir, cria) e seta o valor dele para 200*/
-        firebaseReferencia.child("pontos").setValue("200");
+        // firebasefirebaseDBReferencia.child("pontos").setValue("250");
+
+        //usuarioDBReferencia.child("003").child("nome").setValue("Betânia Silva");
 
 
+        Usuario usuario = new Usuario();
+        usuario.setNome("Beco");
+        usuario.setSobrenome("Silva");
+        usuario.setIdade(45);
+        usuario.setSexo("Masculino");
+
+        usuariosReferenciaFB.child("002").setValue(usuario);
+
+        Produto produto = new Produto();
+        produto.setCor("Preto");
+        produto.setDescricao("Notebook Inspiron");
+        produto.setFabricante("DELL");
+
+        produtosReferenciaFB.child("002").setValue(produto);
     }
 }
