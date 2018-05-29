06ac7f6743cc6d5fdd68972140f7754ab3697e21
Allan Santos <allan.allan.santos@gmail.com>
2018-05-29 17:32:05 -0300

    flappyBird (I)

--- apps-curso/FlappyBird/core/src/edu/allan/flappybird/FlappyBird.java
+++ apps-curso/FlappyBird/core/src/edu/allan/flappybird/FlappyBird.java
@@ -7,27 +7,37 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+    private SpriteBatch batch;
+    private Texture passaro;
+
+//    private int contador = 0;
+
+    @Override
+    public void create() {
+//        Gdx.app.log("Create", "Início do jogo");
+
+        batch = new SpriteBatch();
+        passaro = new Texture("passaro1.png");
+    }
+
+    @Override
+    public void render() {
+
+        /* Corrigir problema de aparecimento de vários pássaros ;) */
+//        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//        contador++;
+//        Gdx.app.log("Render", "Renderizando o jogo: " + contador);
+
+        batch.begin();
+
+        batch.draw(passaro, 150, 800);
+
+        batch.end();
+
+    }
+
+
 }
